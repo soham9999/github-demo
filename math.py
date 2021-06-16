700c347ccfa6077209d0e55f46ff08531642fbec
soham9999 <68984601+soham9999@users.noreply.github.com>
2021-06-16 14:58:10 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# implementation
+def add(x,y):
+    return (x+y)
+def subtract(x,y):
+    return (x-y)
+def multiply(x,y):
+    return(x*y)
+
+# divide implementation
+def divide(x,y):
+    if y==0
+       return DIVIDE_BY_0_ERROR
+    else
+       return (x/y)
+
